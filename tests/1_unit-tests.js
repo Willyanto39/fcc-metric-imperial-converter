90da38e02cd73ebf223630eb7670daefb1aa8848
Willyanto <willyanto39@gmail.com>
2021-06-27 22:01:10 +0700

    Add test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,143 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+  suite('Test getNum()', function() {
+    test('Whole Number Input', function(done) {
+      const num = convertHandler.getNum('5kg');
 
+      assert.equal(num, 5);
+      done();
+    });
+
+    test('Decimal Input', function(done) {
+      const num = convertHandler.getNum('5.6kg');
+
+      assert.equal(num, 5.6);
+      done();
+    });
+
+    test('Fractional Input', function(done) {
+      const num = convertHandler.getNum('6/4kg');
+
+      assert.equal(num, 6 / 4);
+      done();
+    });
+
+    test('Fractional Input with Decimal', function(done) {
+      const num = convertHandler.getNum('5.0/2.0kg');
+
+      assert.equal(num, 5.0 / 2.0);
+      done();
+    });
+
+    test('Double Fraction Input', function(done) {
+      const num = convertHandler.getNum('3/7.2/4kg');
+
+      assert.isUndefined(num);
+      done();
+    });
+
+    test('No Numerical Input', function(done) {
+      const num = convertHandler.getNum('kg');
+
+      assert.equal(num, 1);
+      done();
+    });
+  });
+
+  suite('Test convertHandler.getUnit()', function() {
+    test('For Each Valid Unit Inputs', function(done) {
+      const inputs = ['KG', 'LBS', 'l', 'GAL', 'KM', 'MI'];
+      const outputs = ['kg', 'lbs', 'L', 'gal', 'km', 'mi'];
+
+      inputs.map(function(unit, index) {
+        const result = convertHandler.getUnit(unit);
+        assert.equal(result, outputs[index]);
+      });
+
+      done();
+    });
+
+    test('Invalid Unit Input', function(done) {
+      const unit = convertHandler.getUnit('3cm');
+
+      assert.isUndefined(unit, 'Unit is not undefined');
+      done();
+    });
+  });
+
+  suite('Test convertHandler.getReturnUnit()', function(done) {
+    test('For Each Valid Unit Inputs', function(done) {
+      const inputs = ['kg', 'lbs', 'L', 'gal', 'km', 'mi'];
+      const outputs = ['lbs', 'kg', 'gal', 'L', 'mi', 'km'];
+
+      inputs.map(function(unit, index) {
+        const output = convertHandler.getReturnUnit(unit);
+
+        assert.equal(output, outputs[index]);
+      });
+
+      done();
+    });
+  });
+
+  suite('Test convertHandler.spellOutUnit', function() {
+    test('For Each Valid Unit Inputs', function(done) {
+      const inputs = ['kg', 'lbs', 'L', 'gal', 'km', 'mi'];
+      const outputs = ['kilograms', 'pounds', 
+        'liters', 'gallons', 'kilometers', 'miles'];
+
+      inputs.map(function(unit, index) {
+        const output = convertHandler.spellOutUnit(unit);
+
+        assert.equal(output, outputs[index]);
+      });
+
+      done();
+    });
+  });
+
+  suite('Test convertHandler.convert()', function(done) {
+    test('Convert gal to L', function(done) {
+      const result = convertHandler.convert(1, 'gal');
+
+      assert.approximately(result, 3.78541, 0.1);
+      done();
+    });
+
+    test('Convert L to gal', function(done) {
+      const result = convertHandler.convert(3.78541, 'L');
+
+      assert.approximately(result, 1, 0.1);
+      done();
+    });
+
+    test('Convert mi to km', function(done) {
+      const result = convertHandler.convert(1, 'mi');
+
+      assert.approximately(result, 1.60934, 0.1);
+      done();
+    });
+
+    test('Convert km to mi', function(done) {
+      const result = convertHandler.convert(1.60934, 'km');
+
+      assert.approximately(result, 1, 0.1);
+      done();
+    });
+
+    test('Convert lbs to kg', function(done) {
+      const result = convertHandler.convert(1, 'lbs');
+
+      assert.approximately(result, 0.453592, 0.1);
+      done();
+    });
+
+    test('Convert kg to lbs', function(done) {
+      const result = convertHandler.convert(0.453592, 'kg');
+
+      assert.approximately(result, 1, 0.1);
+      done();
+    });
+  });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,72 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  suite('Test GET /api/convert', function() {
+    test('Convert 10L', function(done) {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: '10L' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 10);
+          assert.equal(res.body.initUnit, 'L');
+          assert.approximately(res.body.returnNum, 2.64172, 0.1);
+          assert.equal(res.body.returnUnit, 'gal');
+          done();
+        });
+    });
 
+    test('Convert 32g', function(done) {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: '32g' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isUndefined(res.body.initUnit);
+          done();
+        });
+    });
+
+    test('Convert 3/7.2/4kg', function(done) {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: '3/7.2/4kg' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isUndefined(res.body.initNum);
+          done();
+        });
+    });
+
+    test('Convert 3/7.2/4kilomegagram', function(done) {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: '3/7.2/4kilomegagram' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.isUndefined(res.body.initNum);
+          assert.isUndefined(res.body.initUnit);
+          done();
+        });
+    });
+
+    test('Convert kg', function(done) {
+      chai
+        .request(server)
+        .get('/api/convert')
+        .query({ input: 'kg' })
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.initNum, 1);
+          assert.equal(res.body.initUnit, 'kg');
+          assert.approximately(res.body.returnNum, 2.20462, 0.1);
+          assert.equal(res.body.returnUnit, 'lbs');
+          done();
+        });
+    });
+  });
 });
